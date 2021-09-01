6bdd441717a2449e7a30c438809fb9457e7522f6
Roberto Oliveira <hetfield_71@hotmail.com>
2021-09-01 10:16:46 +0100

    add products app, models and fixtures

--- T4H_Portal/settings.py
+++ T4H_Portal/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',    
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -152,8 +153,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/products/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/products')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -53,7 +53,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
+        elements: '#media a.media-box'
     });
 
 });

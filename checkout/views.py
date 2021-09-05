17db78fb5a52538c9687f1482348082e788cc70b
Roberto Oliveira <hetfield_71@hotmail.com>
2021-09-05 21:48:17 +0100

    Built checkout sucess layout page

--- checkout/views.py
+++ checkout/views.py
@@ -32,7 +32,6 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
-        print(order_form)
         if order_form.is_valid():
             order = order_form.save()
             for item_id, item_data in bag.items():

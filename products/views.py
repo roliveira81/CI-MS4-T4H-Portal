65a52d90ab7a9b471a9659e35f76648bca21cee5
Roberto Oliveira <hetfield_71@hotmail.com>
2021-09-06 23:34:31 +0100

    Implemented edit product functionality

--- products/views.py
+++ products/views.py
@@ -9,9 +9,10 @@ from .forms import ProductForm
 # Create your views here.
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to return all products - store page """
 
-    products = Product.objects.all()
+    # get only available products in stock
+    products = Product.objects.filter(stock__gt=0)
     query = None
     categories = None
     sort = None
@@ -86,6 +87,30 @@ def add_product(request):
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)

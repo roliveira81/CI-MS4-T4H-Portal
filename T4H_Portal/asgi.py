006abcdaba535303c19d20f0cc487409e6e0f107
Roberto Oliveira <hetfield_71@hotmail.com>
2021-09-07 01:54:17 +0100

    Fixed app name

--- T4H_Portal/asgi.py
+++ T4H_Portal/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'T4H_Portal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 't4h_portal.settings')
 
 application = get_asgi_application()

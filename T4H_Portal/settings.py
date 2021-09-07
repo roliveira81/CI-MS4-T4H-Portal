57f93c76a3aa6686cbaf1712df93190043b9590b
Roberto Oliveira <hetfield_71@hotmail.com>
2021-09-07 01:23:23 +0100

    Connected local env to heroku

--- T4H_Portal/settings.py
+++ T4H_Portal/settings.py
@@ -30,8 +30,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['t4h-portal-app.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,14 +123,17 @@ WSGI_APPLICATION = 'T4H_Portal.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

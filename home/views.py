5aee69400893c63adc0933c7414fbaac93f76172
Roberto Oliveira <hetfield_71@hotmail.com>
2021-09-08 02:43:11 +0100

    Added shows list on home view functionality

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from shows.models import Show
 
 from django.conf import settings
 from django.http import HttpResponse
 from django.core.mail import send_mail, BadHeaderError
 from django.contrib import messages
+from datetime import datetime
 
 
 def index(request):
@@ -11,6 +13,14 @@ def index(request):
     A view to return index page
     Contact form handler 
     """
+
+    # returns just upcoming dates
+    shows = Show.objects.all().filter(date__gte=datetime.now())
+
+    context = {
+        'shows': shows
+    }
+
     if request.method == 'POST':
         email = request.POST['email']
         name = request.POST['name']
@@ -36,4 +46,4 @@ def index(request):
 
     template = 'home/index.html'
 
-    return render(request, template)
+    return render(request, template, context)
